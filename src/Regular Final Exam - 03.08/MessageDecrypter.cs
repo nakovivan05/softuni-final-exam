b211d3ad32b7c99b142dbb821c20f90de771d610
ivannakov05 <ivannakov11@gmail.com>
2025-08-08 18:35:35 +0300

    feat(Regular Final Exam): add Message Decrypter exercise solution

---
+++ src/Regular Final Exam - 03.08/MessageDecrypter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+class Program
+{
+    static void Main()
+    {
+        int numberOfInputs = int.Parse(Console.ReadLine());
+        string pattern = @"^([$%])([A-Z][a-z]{2,})\1: \[([0-9]+)\]\|\[([0-9]+)\]\|\[([0-9]+)\]\|$";
+        for (int i = 0; i < numberOfInputs; i++)
+        {
+            string message = Console.ReadLine();
+            Match match = Regex.Match(message, pattern);
+            if (match.Success)
+            {
+                string tag = match.Groups[2].Value;
+                char symbol1 = (char)int.Parse(match.Groups[3].Value);
+                char symbol2 = (char)int.Parse(match.Groups[4].Value);
+                char symbol3 = (char)int.Parse(match.Groups[5].Value);
+                string decryptedMessage = $"{tag}: {symbol1}{symbol2}{symbol3}";
+                Console.WriteLine(decryptedMessage);
+            }
+            else
+            {
+                Console.WriteLine("Valid message not found!");
+            }
+        }
+    }
+}
